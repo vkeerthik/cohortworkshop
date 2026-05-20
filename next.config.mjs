3e07f895d24a1e97e2fd2d636f96d0b7b4641c6e
vkeerthik <venkata.keerthi@cabotsolutions.com>
2026-05-20 12:07:27 -0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -4,15 +4,15 @@ const isProd = process.env.NODE_ENV === "production";
 // Set this to your GitHub repo name (the part after username.github.io/)
 // e.g. if your repo is "cohort-workshop-platform", leave as is.
 // If you deploy to a user/org page (username.github.io with no subpath), set REPO_NAME to "".
-const REPO_NAME = "cohort-workshop-platform";
+const REPO_NAME = "cohortworkshop";
 
 const nextConfig = {
   reactStrictMode: true,
   output: "export",
   images: { unoptimized: true },
   trailingSlash: true,
-  basePath: isProd && cohortworkshop ? `/${cohortworkshop}` : "",
-  assetPrefix: isProd && cohortworkshop ? `/${cohortworkshop}/` : "",
+  basePath: isProd && REPO_NAME ? `/${REPO_NAME}` : "",
+  assetPrefix: isProd && REPO_NAME ? `/${REPO_NAME}/` : "",
 };
 
 export default nextConfig;
